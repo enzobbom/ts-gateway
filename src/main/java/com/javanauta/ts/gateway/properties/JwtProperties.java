ac0dbc67b7472183ad9b3cc6be99644ff059f354
Enzo Bom <enzobom@gmail.com>
2026-07-31 21:38:13 +0100

    Final application properties refactor before first deploy (including removal of unused JwtExpiration)

--- src/main/java/com/javanauta/ts/gateway/properties/JwtProperties.java
+++ src/main/java/com/javanauta/ts/gateway/properties/JwtProperties.java
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "ts.jwt")
 public record JwtProperties(
-        String secret,
-        long expiration
+        String secret
 ) {
 }
